62f6605fce52ef8f6c7aa07ece699014dd4260e9
ivnmrtk <ivnmrtk@gmail.com>
2023-11-14 14:13:48 +0300

    fix crashing, add basic test /OPS-399

---
+++ src/test/java/com/rbkmoney/proxy/mocketbank/mpi/utils/ProxyMocketBankTest.java
@@ -0,0 +1,14 @@
+package com.rbkmoney.proxy.mocketbank.mpi.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProxyMocketBankTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
